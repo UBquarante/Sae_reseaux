2ec54ff33c897636f0674b14e550a06b8e0e4e93
UBquarante <lucasleroux24@icloud.com>
2025-05-18 20:15:19 +0200

    Ajoutez des fichiers projet.

---
+++ Sae_reseaux/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Sae_reseaux
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTitre = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            label4 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblTitre
+            // 
+            lblTitre.AutoSize = true;
+            lblTitre.Font = new Font("Segoe UI", 20F);
+            lblTitre.Location = new Point(255, 24);
+            lblTitre.Name = "lblTitre";
+            lblTitre.Size = new Size(314, 37);
+            lblTitre.TabIndex = 0;
+            lblTitre.Text = "Calculateur de Checksum";
+            lblTitre.Click += label1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 13F);
+            label1.Location = new Point(12, 97);
+            label1.Name = "label1";
+            label1.Size = new Size(188, 175);
+            label1.TabIndex = 1;
+            label1.Text = "Groupe F3\r\n\r\nLescot Oscar \r\nLe Callennec Julie\r\nLagouge Jeanne\r\nBourel Hecquet Scotty\r\nLeroux Lucas\r\n";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 13F);
+            label2.Location = new Point(12, 291);
+            label2.Name = "label2";
+            label2.Size = new Size(159, 100);
+            label2.TabIndex = 2;
+            label2.Text = "Année 2024/2025 \r\nIUT Amiens\r\nSAE 2.3\r\n\r\n";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 13F);
+            label3.Location = new Point(64, 51);
+            label3.Name = "label3";
+            label3.Size = new Size(233, 50);
+            label3.TabIndex = 3;
+            label3.Text = "Zone de saisie du checksum\r\n\r\n";
+            label3.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 13F);
+            button1.Location = new Point(497, 276);
+            button1.Name = "button1";
+            button1.Size = new Size(113, 34);
+            button1.TabIndex = 4;
+            button1.Text = "Calculer";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 13F);
+            label4.Location = new Point(104, 25);
+            label4.Name = "label4";
+            label4.Size = new Size(157, 25);
+            label4.TabIndex = 5;
+            label4.Text = "Resultat checksum";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ControlDark;
+            panel1.Controls.Add(label3);
+            panel1.Location = new Point(369, 97);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(360, 130);
+            panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ControlDark;
+            panel2.Controls.Add(label4);
+            panel2.Location = new Point(369, 350);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(360, 69);
+            panel2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(lblTitre);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTitre;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+        private Label label4;
+        private Panel panel1;
+        private Panel panel2;
+    }
+}
