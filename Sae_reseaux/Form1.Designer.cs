498207968a6136d9f24e48df4923eb98763b3f84
Oscar <lescotoscar@gmail.com>
2025-05-19 11:35:17 +0200

    Ajout d'un sous programme CalculSommeHexa

--- Sae_reseaux/Form1.Designer.cs
+++ Sae_reseaux/Form1.Designer.cs
@@ -44,9 +44,9 @@
             // 
             lblTitre.AutoSize = true;
             lblTitre.Font = new Font("Segoe UI", 20F);
-            lblTitre.Location = new Point(255, 24);
+            lblTitre.Location = new Point(291, 32);
             lblTitre.Name = "lblTitre";
-            lblTitre.Size = new Size(314, 37);
+            lblTitre.Size = new Size(396, 46);
             lblTitre.TabIndex = 0;
             lblTitre.Text = "Calculateur de Checksum";
             lblTitre.Click += label1_Click;
@@ -55,9 +55,9 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 13F);
-            label1.Location = new Point(12, 97);
+            label1.Location = new Point(14, 129);
             label1.Name = "label1";
-            label1.Size = new Size(188, 175);
+            label1.Size = new Size(230, 210);
             label1.TabIndex = 1;
             label1.Text = "Groupe F3\r\n\r\nLescot Oscar \r\nLe Callennec Julie\r\nLagouge Jeanne\r\nBourel Hecquet Scotty\r\nLeroux Lucas\r\n";
             // 
@@ -65,9 +65,9 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 13F);
-            label2.Location = new Point(12, 291);
+            label2.Location = new Point(14, 388);
             label2.Name = "label2";
-            label2.Size = new Size(159, 100);
+            label2.Size = new Size(192, 120);
             label2.TabIndex = 2;
             label2.Text = "Année 2024/2025 \r\nIUT Amiens\r\nSAE 2.3\r\n\r\n";
             label2.Click += label2_Click;
@@ -76,9 +76,9 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 13F);
-            label3.Location = new Point(64, 51);
+            label3.Location = new Point(73, 68);
             label3.Name = "label3";
-            label3.Size = new Size(233, 50);
+            label3.Size = new Size(283, 60);
             label3.TabIndex = 3;
             label3.Text = "Zone de saisie du checksum\r\n\r\n";
             label3.Click += label3_Click;
@@ -86,9 +86,10 @@
             // button1
             // 
             button1.Font = new Font("Segoe UI", 13F);
-            button1.Location = new Point(497, 276);
+            button1.Location = new Point(568, 368);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(113, 34);
+            button1.Size = new Size(129, 45);
             button1.TabIndex = 4;
             button1.Text = "Calculer";
             button1.UseVisualStyleBackColor = true;
@@ -97,9 +98,9 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 13F);
-            label4.Location = new Point(104, 25);
+            label4.Location = new Point(119, 33);
             label4.Name = "label4";
-            label4.Size = new Size(157, 25);
+            label4.Size = new Size(189, 30);
             label4.TabIndex = 5;
             label4.Text = "Resultat checksum";
             // 
@@ -107,33 +108,37 @@
             // 
             panel1.BackColor = SystemColors.ControlDark;
             panel1.Controls.Add(label3);
-            panel1.Location = new Point(369, 97);
+            panel1.Location = new Point(422, 129);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(360, 130);
+            panel1.Size = new Size(411, 173);
             panel1.TabIndex = 6;
             // 
             // panel2
             // 
             panel2.BackColor = SystemColors.ControlDark;
             panel2.Controls.Add(label4);
-            panel2.Location = new Point(369, 350);
+            panel2.Location = new Point(422, 467);
+            panel2.Margin = new Padding(3, 4, 3, 4);
             panel2.Name = "panel2";
-            panel2.Size = new Size(360, 69);
+            panel2.Size = new Size(411, 92);
             panel2.TabIndex = 7;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(914, 600);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(button1);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(lblTitre);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             panel2.ResumeLayout(false);
--- Sae_reseaux/Form1.cs
+++ Sae_reseaux/Form1.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic;
+
 namespace Sae_reseaux
 {
     public partial class Form1 : Form
@@ -7,10 +9,22 @@ namespace Sae_reseaux
             InitializeComponent();
 
         }
+        string CalculSommeHexa(string[] TableauxValeur)
+        {
+            string resultat = "0";
+            int somme = 0;
+            foreach (string s in TableauxValeur)
+            {
+                somme = Convert.ToInt32(resultat, 16) +
+                    Convert.ToInt32(s, 16);
+                resultat = Conversion.Hex(somme);
+            }
+            return resultat;
+        }
+
 
         private void label1_Click(object sender, EventArgs e)
         {
-
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -19,6 +33,11 @@ namespace Sae_reseaux
         }
 
         private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
